e3ecc8868584d861e384b39eebfc04032d389caa
Pradeep-React-Node <dev.react@vibgyorweb.com>
2024-12-19 22:55:04 +0530

    url change

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-// export const baseUrl = "https://dev.43.204.163.128.nip.io/api/v1/expo";
+export const baseUrl = "https://dev.43.204.163.128.nip.io/api/v1/expo";
 
-export const baseUrl = "http://localhost:4001/api/v1/expo";
+// export const baseUrl = "http://localhost:4001/api/v1/expo";
 // router.route("/register").post(registerExpoUser )
 // router.route("/allusers").get(getAllExpoUsers)
 // router.route("/user/:id").get(getUserById)
